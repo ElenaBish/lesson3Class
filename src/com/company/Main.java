d275f3fb9cfe05a6315b1453d44bfca990e76aec
Elena Bishtankovskaya <elena.iv.bish@gmail.com>
2019-12-06 15:29:38 +0600

    create class User and Profile/add LinkedList

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Profile operator=new Profile();
+        operator.firstName="Ivanov";
+        operator.lastName="Ivan";
+        operator.age=25;
+        operator.user.login="IvIv";
+        operator.user.password="123";
+
+        LinkedList<Integer>linkedList=new LinkedList<>();
+        linkedList.add(7);
+
+    }
+
+}
---
+++ src/com/company/Profile.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Profile {
+    String firstName;
+    String lastName;
+    int age;
+    User user;
+
+}
